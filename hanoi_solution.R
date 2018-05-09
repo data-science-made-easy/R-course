dfcf646313c2ca9330a51af8f5c3a7e28c7603b6
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2018-05-09 10:03:01 +0200

    Made recursion easier: base case is now zero disks.

--- hanoi_solution.R
+++ hanoi_solution.R
@@ -1,34 +1,33 @@
-rm(list=ls()) # Remove all objecs, be sure you work with a clean environment
+rm(list=ls())
 
 MoveTower = function(n, source, dest, extra, tower)
 {
-	# MoveTower moves n disks from pilar source to pilar dest via pilar 'extra' in tower 'tower'
-	if (n == 1)
-	{
-		# get the disk
-		disk = tower[[source]][1]
+	if (n == 0)	{
+    
+    # Base case: do nothing
+    
+	} else { # 0 < n
+    
+    # First move the top n - 1 disks from 'source' ==> 'extra'
+		tower <- MoveTower(n - 1, source = source, dest = extra, extra = dest, tower = tower)
 
-		# print how to move disk
+    # Now move disk at bottom from 'source' ==> 'dest'
+		disk            <- tower[[source]][1]     # get the disk from source
+		tower[[source]] <- tower[[source]][-1]    # remove disk from source
+		tower[[dest]]   <- c(disk, tower[[dest]]) # prepend disk to dest
+
+    # print what we did
 		cat("Move disk", disk, "from", LETTERS[source], "to", LETTERS[dest], "\n")
-		
-		# prepend disk to dest
-		tower[[dest]] = c(disk, tower[[dest]])
-		
-		# remove disk from source
-		tower[[source]] = tower[[source]][-1]
-	}
-	else # 1 < n
-	{
-		tower = MoveTower(n - 1, source, extra, dest, tower)
-		tower = MoveTower(1, source, dest, extra, tower)
-		tower = MoveTower(n - 1, extra, dest, source, tower)
+    
+    # Last move the top n - 1 disks from 'extra' ==> 'dest'
+		tower <- MoveTower(n - 1, source = extra, dest = dest, extra = source, tower = tower)
 	}
 	
 	# return our new tower
-	return( tower )
+	return(tower)
 }
 
 
-n = 3
-tower = list(1:n, c(), c())
-MoveTower(n , 1, 2, 3, tower)
+n <- 3
+tower <- list(1:n, c(), c())
+MoveTower(n, source = 1, dest = 2, extra = 3, tower)
